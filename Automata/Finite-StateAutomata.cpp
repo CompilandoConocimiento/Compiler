6296a86ad50a3d8c29ff0ffc4b99847547c3ac3a
Oscar Andrés Rosas <soyoscarrh@gmail.com>
2018-08-21 20:15:42 -0500

    Simple update

--- Automata/Finite-StateAutomata.cpp
+++ Automata/Finite-StateAutomata.cpp
@@ -4,23 +4,37 @@
 
 using namespace std;
 
+template <class id, class symbol>
 struct state {                          
-    unsigned int id; 
-    map<int, unsigned int> transtition;
+    id name; 
+    map<id, symbol> transtition;
     bool isFinalState;              
 };
 
+template <class id, class symbol>
 class FiniteStateAutomata {
+    using state = state<id, symbol>;
 
     private:
         vector<state> states;
         state *initialState;
-        vector<char> alphabelt;
+        set<symbol> alphabelt;
 
     public:
-        FiniteStateAutomata(
-            vector<state> states, state* initialState,
-            vector<char> alphabelt
-        );
-
+        FiniteStateAutomata(vector<state> states, state* initialState, set<symbol> alphabelt) {
+            this.states = states;
+            this.states = states;
+        }
 };
+
+
+int main () {
+    
+    using u_int = unsigned int;
+    using symbol = char;
+
+    auto a = state<u_int, symbol>()
+
+
+    return 0;
+}
